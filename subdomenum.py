a7e4b6d2e3c00f6fe876f2b4e804ec7f48c44666
Canister88 <57096867+Canister88@users.noreply.github.com>
2024-12-06 20:13:19 +0530

    Update subdomenum.py

--- subdomenum.py
+++ subdomenum.py
@@ -2,3 +2,13 @@ import dns.zone
 import dns.node
 import dns.query
 
+domain = str(input('Enter domain name for which to find subdomains '))
+dns_server = str(input('Enter dns server ip address '))
+
+def zone_transfer_check (domain, dns_server):
+  try:
+    print (f"Checking {dns_server} for zone transfer on {domain}...")
+    zone = dns.zone.from_xfr(dns.query.xfr(dns_server, domain))
+    if zone:
+      print (f"Zone transfer available on {dns_server} !!")
+      
